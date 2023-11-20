15a290180acb29e43d9050f882f70c33ec05136f
tteokbokki-master <0jin.git@gmail.com>
2023-11-20 17:15:30 +0900

    feat: Todo 완료 시 취소선

--- src/Components/Main/TodoItem.jsx
+++ src/Components/Main/TodoItem.jsx
@@ -1,10 +1,9 @@
 import './TodoItem.css'
 
-
-
-
-
 export default function TodoItem({id,isDone,content,onClickCheckBox,onClickDeleteTodo}){
+  
+  const TodoItem_content = isDone ? 'TodoItem_content_true' : 'TodoItem_content_false'
+
   const onChangeCheckBox = () =>{
     onClickCheckBox(id)
     console.log(id)
@@ -13,13 +12,15 @@ export default function TodoItem({id,isDone,content,onClickCheckBox,onClickDelet
   const onClickDelete = () =>{
     onClickDeleteTodo(id)
   }
+  
+
   return(
     <div className="TodoItem">
       <input onChange={onChangeCheckBox} id={`TodoItem-checkbox-${id}`} checked={isDone}type='checkbox'/>
       <label htmlFor={`TodoItem-checkbox-${id}`}/>
-      <p className='TodoItem-content'>{content}</p>
+      <p className={TodoItem_content}>{content}</p>
       <button className='TodoItem-EdiBtn'>수정</button>
       <button className='TodoItem-DelBtn' onClick={onClickDelete}>삭제</button>
     </div>
   )
-}
+  }

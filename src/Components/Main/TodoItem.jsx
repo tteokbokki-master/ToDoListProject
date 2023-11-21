2db6512ebbf9aef670f039509866f1ce8ee88a0a
tteokbokki-master <0jin.git@gmail.com>
2023-11-21 18:00:11 +0900

    input 수정칸 생성

--- src/Components/Main/TodoItem.jsx
+++ src/Components/Main/TodoItem.jsx
@@ -1,26 +1,43 @@
+import { useState } from 'react'
 import './TodoItem.css'
 
 export default function TodoItem({id,isDone,content,onClickCheckBox,onClickDeleteTodo}){
-  
+
   const TodoItem_content = isDone ? 'TodoItem_content_true' : 'TodoItem_content_false'
 
+  const [onOff, setOnOFF] = useState(false)
+
   const onChangeCheckBox = () =>{
+    if(onOff===false){
     onClickCheckBox(id)
-    console.log(id)
-    console.log(content)
+    }
   }
   const onClickDelete = () =>{
     onClickDeleteTodo(id)
   }
-  
+
+  const onClickonOff = () =>{
+    setOnOFF(!onOff)
+  }
 
   return(
     <div className="TodoItem">
       <input onChange={onChangeCheckBox} id={`TodoItem-checkbox-${id}`} checked={isDone}type='checkbox'/>
       <label htmlFor={`TodoItem-checkbox-${id}`}/>
-      <p className={TodoItem_content}>{content}</p>
-      <button className='TodoItem-EdiBtn'>수정</button>
-      <button className='TodoItem-DelBtn' onClick={onClickDelete}>삭제</button>
+      
+      { isDone===false && onOff ? 
+          <div className='trueItem'>
+          <input className='TodoItem-input' placeholder={content}/>
+          <button className='TodoItem-EdiBtn'>수정완료</button>
+          <button className='TodoItem-DelBtn' onClick={onClickonOff}>닫기</button> 
+        </div> 
+      :
+        <div className='falseItem'>
+          <p className={TodoItem_content}>{content}</p> 
+          <button className='TodoItem-EdiBtn' onClick={onClickonOff}>수정</button>
+          <button className='TodoItem-DelBtn' onClick={onClickDelete}>삭제</button> 
+      </div>
+      }
     </div>
   )
   }

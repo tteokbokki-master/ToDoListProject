d26b61d645a88fec61c812e23a7346c19550d8af
tteokbokki-master <0jin.git@gmail.com>
2023-11-21 21:49:29 +0900

    feat: 투두리스트 localStorage 저장 구현

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,12 +3,29 @@ import Header from '../Components/Main/Header'
 import TodoEditor from '../Components/Main/TodoEditor'
 import TodoList from '../Components/Main/TodoList'
 import TodoCheck from '../Components/Main/TodoCheck'
-import { useState,useRef } from 'react'
+import { useState,useRef,useEffect } from 'react'
 
 export default function Home(){
 
-  const [todos,setTodos]= useState([]);
-  const currentId = useRef(0);
+  const localTodos = window.localStorage.getItem("todos")
+  ? JSON.parse(window.localStorage.getItem("todos"))
+  : [];
+  
+  const [todos,setTodos]= useState(localTodos);
+  
+  useEffect(()=>{
+    window.localStorage.setItem("todos",JSON.stringify(todos))
+    
+  },[todos])
+
+  const [idCount,setIdCount] = useState(0);
+
+  useEffect(()=>{
+    const data = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : 0
+    const dataLength = Object.keys(data).length;
+    setIdCount(dataLength)
+  },[todos])
+  
 
   const isDone_O = todos.filter((todo)=>{
     return todo.isDone != false
@@ -21,6 +38,7 @@ export default function Home(){
     setTodos(todos.map((todo)=>todo.id === currentId ? {...todo, isDone: !todo.isDone} : todo))
   }
 
+
   const onClickDeleteTodo = (currentId) => {
     setTodos(todos.filter((todo)=> todo.id != currentId))
   }
@@ -28,19 +46,18 @@ export default function Home(){
   const onEditTodo = (currentId,currentContent) => {
     setTodos(todos.map((todo)=> todo.id === currentId ? {...todo, content : currentContent} : todo))
   }
-  console.log(todos)
+
   const onClickAddTodos = (content) =>{
     const data = {
-      id : currentId.current++,
+      id : idCount,
       isDone : false,
       content : content,
     }
     setTodos([...todos,data])
   }
 
+
   
-
-
   return(
     <div className="Home">
       <Header/>
